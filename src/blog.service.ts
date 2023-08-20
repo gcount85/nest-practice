7cbe47861a4bc8817bbf3889af7ee9f1a835d942
gcount85 <100mgml@gmail.com>
2023-08-20 16:16:04 +0900

    feat : Init basic blog service

--- src/blog.service.ts
+++ src/blog.service.ts
@@ -1,1 +1,34 @@
-export class BlogService {}
+import { PostDto } from './blog.model';
+
+export class BlogService {
+  posts = [];
+
+  getAllPosts() {
+    return this.posts;
+  }
+
+  createPost(postDto: PostDto) {
+    const id = this.posts.length + 1;
+    this.posts.push({ id: id.toString(), ...postDto, createdDt: new Date() });
+    return id;
+  }
+
+  getPost(id: string) {
+    const post = this.posts.find((post) => post.id === id);
+    console.log(post);
+    return post;
+  }
+
+  deletePost(id: string) {
+    const filteredPosts = this.posts.filter((post) => post.id !== id);
+    this.posts = [...filteredPosts];
+    return id;
+  }
+
+  updatePost(id: string, postDto: PostDto) {
+    let updateIndex = this.posts.findIndex((post) => post.id === id);
+    const updatePost = { id, ...postDto, updateDt: new Date() };
+    this.posts[updateIndex] = updatePost;
+    return updatePost;
+  }
+}
