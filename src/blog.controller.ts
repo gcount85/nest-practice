5802b57b4e52fcf7a80af84bf9af270b34b7ca18
gcount85 <100mgml@gmail.com>
2023-08-20 17:09:14 +0900

    feat : Import service to controller

--- src/blog.controller.ts
+++ src/blog.controller.ts
@@ -7,31 +7,42 @@ import {
   Delete,
   Controller,
 } from '@nestjs/common';
+import { BlogService } from './blog.service';
 
 @Controller('blog')
 export class BlogController {
+  blogService: BlogService;
+  constructor() {
+    // 생성자로 블로그 서비스 객체 생성. 의존성 주입으로 대체할 수 있음.
+    this.blogService = new BlogService();
+  }
+
   @Get()
   getAllPosts() {
-    return;
+    return this.blogService.getAllPosts();
   }
 
   @Post()
-  createPost(@Body() post: any) {
-    console.log(post);
+  createPost(@Body() postDto: any) {
+    console.log(postDto);
+    return this.blogService.createPost(postDto);
   }
 
   @Get(':id')
   getPost(@Param('id') id: string) {
     console.log(`[id: ${id}] 게시글 하나 가져오기`);
+    return this.blogService.getPost(id);
   }
 
   @Delete(':id')
   deletePost(@Param('id') id: string) {
     console.log(`[id: ${id}] 게시글 하나 삭제`);
+    return this.blogService.deletePost(id);
   }
 
   @Put(':id')
-  updatePost(@Param('id') id: string, @Body() post: any) {
-    console.log(`[id: ${id}] 게시글 업데이트 내용 ${post}`);
+  updatePost(@Param('id') id: string, @Body() postDto: any) {
+    console.log(`[id: ${id}] 게시글 업데이트 내용 ${postDto}`);
+    return this.blogService.updatePost(id, postDto);
   }
 }
