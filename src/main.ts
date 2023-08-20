6371d680b691b3381fcf55cc53a6f75ab163001f
gcount85 <100mgml@gmail.com>
2023-08-20 15:06:49 +0900

    init basic service and contorller

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(3000, () => {
+    console.log('자 이제 시작이야 nest~');
+  });
 }
 bootstrap();
