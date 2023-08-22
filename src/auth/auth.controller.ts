c61dd06e3949d3118988039d2d7db58e14934627
100mgml@gmail.com <100mgml@gmail.com>
2023-08-22 17:48:28 +0900

    feat : Implement register method in auth module

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,25 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { User } from 'src/user/user.entity';
+import { CreateUserDto } from 'src/user/user.dto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('register')
+  // CreateUserDto를 사용하면 cross-validator가 자동으로 유효성 검증한다.
+  async register(@Body() createUserDto: CreateUserDto): Promise<User> {
+    return await this.authService.register(createUserDto);
+  }
+
+  @Post('login')
+  async login() {
+    return await this.authService.validateUser();
+  }
+
+  @Post('logout')
+  async logout() {
+    return await this.authService.logout();
+  }
+}
