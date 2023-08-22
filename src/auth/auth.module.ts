2e0ed5cc69aed28536e2dfe39271c260cf21985d
100mgml@gmail.com <100mgml@gmail.com>
2023-08-22 18:17:01 +0900

    style : Add comment

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,7 +4,7 @@ import { AuthService } from './auth.service';
 import { UserModule } from 'src/user/user.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule], // UserService를 사용하기 위해 UserModule 자체를 임포트한다.
   controllers: [AuthController],
   providers: [AuthService],
 })
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -8,6 +8,6 @@ import { User } from './user.entity';
   imports: [TypeOrmModule.forFeature([User])], // TypeOrmModule과 User 엔티티 등록
   controllers: [UserController],
   providers: [UserService],
-  exports: [UserService],
+  exports: [UserService], // UserService 모듈을 export하여 AuthModule에서 사용하도록 한다.
 })
 export class UserModule {}
